03fc9eed794ddccec179349ebfdbd1ccee78e586
luke-h1 <luke.howsam@yahoo.com>
2020-12-26 14:31:39 +0000

    Create new doc page, add table of contents

--- packages/gatsby-theme-docs/src/components/docs-page.js
+++ packages/gatsby-theme-docs/src/components/docs-page.js
@@ -1,23 +1,24 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
+import { jsx } from 'theme-ui';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from './layout';
-
+import TableOfContents from './table-of-contents';
 const DocsPage = ({ page }) => (
   <Layout>
     <h1>{page.title}</h1>
     <MDXRenderer>{page.body}</MDXRenderer>
     <p
-    sx={{ 
-      borderTop: theme => `1px solid ${theme.colors.muted}`,
-      color: 'muted',
-      fontSize: 14,
-      mt: 2,
-      pt: 2,
-      
-     }}
+      sx={{
+        borderTop: (theme) => `1px solid ${theme.colors.muted}`,
+        color: 'muted',
+        fontSize: 14,
+        mt: 2,
+        pt: 2,
+      }}
     >
-      This page was updated {page.updated}</p>
+      This page was updated {page.updated}
+    </p>
+    <TableOfContents />
   </Layout>
 );
 export default DocsPage;
