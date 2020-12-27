b0238f2f3a559b1514a85213b55dec346d4eac44
luke-h1 <luke.howsam@yahoo.com>
2020-12-27 17:18:59 +0000

    extending themes with UI variants

--- sites/negronis/gatsby-config.js
+++ sites/negronis/gatsby-config.js
@@ -20,5 +20,12 @@ module.exports = {
         uploadFolder: 'fem-workshop',
       },
     },
+    {
+      resolve: 'gatsby-theme-docs',
+      options: {
+        basePath: '/recipes',
+        contentPath: 'content/recipes',
+      },
+    },
   ],
 };
--- sites/negronis/src/pages/index.js
+++ sites/negronis/src/pages/index.js
@@ -5,28 +5,31 @@ import Image from 'gatsby-image';
 import Layout from '../components/layout';
 
 export const query = graphql`
-query  {
-  image: file(name: { eq: "negroni" }){
-    cloudinary: childCloudinaryAsset {
-      fluid(transformations: ["ar_18:9", "c_fill"]) {
-        ...CloudinaryAssetFluid
+  query {
+    image: file(name: { eq: "negroni" }) {
+      cloudinary: childCloudinaryAsset {
+        fluid(transformations: ["ar_18:9", "c_fill"]) {
+          ...CloudinaryAssetFluid
+        }
       }
     }
   }
-}
-`; 
+`;
 
 const Index = ({ data }) => (
   <Layout>
-    <Image fluid={data.image.cloudinary.fluid} alt='Negroni.' />
+    <Image fluid={data.image.cloudinary.fluid} alt="Negroni." />
     <h1>Negronis: A Love Story</h1>
     <p>
       A Negroni, while simple, is a beautifully complex cocktail with endless
       opportunities to experiment with flavors.
     </p>
-    <Link to="/history"
-      sx={{ variant: 'button.hollow' }}
-    >Learn the History</Link>
+    <Link to="/recipes" sx={{ variant: 'button.primary' }}>
+      See recipes
+    </Link>
+    <Link to="/history" sx={{ variant: 'button.hollow', ml: 3 }}>
+      Learn the History
+    </Link>
   </Layout>
 );
 
