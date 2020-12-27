dce2b66cc98c8367adf306e0344f1ddd646ae2c6
luke-h1 <luke.howsam@yahoo.com>
2020-12-27 20:12:32 +0000

    querying with apollo client

--- sites/lookup/src/pages/search.js
+++ sites/lookup/src/pages/search.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Form from '../components/form';
 import Results from '../components/Results';
 
-const Search = () => (
-  <>
-    <h1>Search for Rick and morty character lookup</h1>
-    <p>
-      trying to remember which rick you're talking about ? try out this handy
-      search interface !
-    </p>
-    <Form />
-    <Results name='rick'/>
-  </>
-);
+const Search = ({ location }) => {
+  const query =
+    (location.state && location.state.query) ||
+    location.pathname.replace(/^\search\/?/, '') ||
+    '';
+  const name = query.replace(/-+/g, ' ');
+  return (
+    <>
+      <h1>
+        {name
+          ? `Showing results for "${name}"`
+          : 'Search for Rick and morty character lookup'}
+      </h1>
+      <p>
+        trying to remember which rick you're talking about ? try out this handy
+        search interface !
+      </p>
+      <Form />
+      {name && <Results name={name} />}
+    </>
+  );
+};
 export default Search;
